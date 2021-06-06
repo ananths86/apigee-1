c9504602cbd2f7579df4b952142001dcb0c9d170
ananths86 <ananthcharm@gmail.com>
2021-06-06 16:39:37 -0400

    Add files via upload
    updated PathVariable

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	
 	@GetMapping("/{name}")
-	public String sayHello(@PathParam("name") String name) {
+	public String sayHello(@PathVariable("name") String name) {
 		return "Hello "  + name;
 	}
 
